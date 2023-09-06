bfb0f541306b2a099707daec5be92e917842ef8d
Isaac Orraiz <isaacorraiz@gmail.com>
2023-09-05 23:51:49 -0400

    All Event listener created, next, local storage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clear = document.getElementById("clear");
+const filter = document.querySelector(".filter");
 
 function createButton(classes) {
   const button = document.createElement("button");
@@ -38,7 +40,52 @@ function addItem(e) {
   itemList.appendChild(li);
 
   itemInput.value = "";
+
+  checkItems();
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+    checkItems();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkItems();
+}
+
+function filterItems(e) {
+  const text = e.target.value.toLowerCase();
+  const items = document.querySelectorAll("li");
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkItems() {
+  if (!itemList.firstChild) {
+    filter.style.display = "none";
+    clear.style.display = "none";
+  } else {
+    filter.style.display = "block";
+    clear.style.display = "block";
+  }
 }
 
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clear.addEventListener("click", clearItems);
+filter.addEventListener("input", filterItems);
+
+checkItems();
